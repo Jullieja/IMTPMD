28e66f413745b52fb18889e4a9e1e1e4b6482017
Julia Rijsbergen <juliarijsbergen@gmail.com>
2017-06-07 14:41:05 +0200

    Van login scherm naar kies een jaar scherm

---
+++ IMTPMD/app/src/main/java/com/example/rick/imtpmd/PickYearActivity.java
@@ -0,0 +1,13 @@
+package com.example.rick.imtpmd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PickYearActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_year);
+    }
+}
